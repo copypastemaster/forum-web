bfac52b085d8258a39d4d37e1efedabcdfd98b97
master <enoch.binas@gmail.com>
2023-08-30 01:02:59 +0800

    save point before messing with things

--- src/components/Homepage/HomeCards.tsx
+++ src/components/Homepage/HomeCards.tsx
@@ -1,13 +1,20 @@
 import { useGetUser } from "@/services/getUsers"
 import { UserTypes } from "@/types/types"
 import { ArrowUp, MessageSquare } from "lucide-react"
-import { useState } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import axios from "axios"
 
 export default function HomeCards() {
 
   const { isLoading, data } = useGetUser();
-  const [vote, setVote] = useState(); 
+  const [vote, setVote] = useState();
+
+  const useFetchUser = () => {
+    
+    useEffect(() => {
+
+    })
+  }
   
   if (isLoading) {
     return <h1>Loading ... </h1>
@@ -16,6 +23,7 @@ export default function HomeCards() {
   return (
     <div className='flex flex-col space-y-10'>
         {data?.data.map((user: UserTypes<string, number>) => {
+
           const votes = {
             "id": user.id,
             "user_name": user.user_name,
@@ -26,25 +34,31 @@ export default function HomeCards() {
               "summary": user.posts.summary,
               "repo": user.posts.repo,
               "live_demo": user.posts.live_demo,
-              "comments": [
-                user.posts.comments
-              ],
+              "comments": user.posts.comments,
               "upvote": user.posts.upvote++
-      }
+               }
+            }
 
-          }
+
             return (
               <div key={user.id} className={`border rounded-md border-cyan-50 max-w-xl mx-auto p-5 shadow-md`}>
                 <img src={user.posts.screenshot} className='mx-auto hover:cursor-pointer'/>
                 <h1 className='text-3xl font-semibold mt-5'>{user.posts.title}</h1>
-                <p className='text-sm font-light mt-2 underline'>{user.user_name}</p>
-                <p className='mt-3'>{user.posts.summary}</p>
+                <p className='text-md font-light mt-2'>{user.user_name}</p>
+
+                <div className='flex flex-col mt-2'>
+                  <a className='text-sm font-normal mt-1 underline' href="#">Repo: {user.posts.repo} </a>
+                  <a className='text-sm font-normal mt-1 underline' href="#">Live: {user.posts.live_demo} </a>
+                </div>
+                
+                <p className='mt-5'>{user.posts.summary}</p>
+
                 <div className='flex gap-5 mt-2'>
                   <section className='flex gap-1 hover: cursor-pointer' onClick={() => {
                     axios.patch(`http://localhost:3000/users/${user.id}/?posts=upvote`, votes)
-                      .then(res => console.log(res.data.posts.upvote++))
+                      .then(res => setVote(res.data.posts.upvote))
                       .catch(err => console.log(err));
-                  }}>
+                  }}>                   
                     <ArrowUp size={18} className="mt-1"/>
                     <p>{user.posts.upvote}</p>
                   </section> 
@@ -53,6 +67,7 @@ export default function HomeCards() {
                     <p>{user.posts.comments.length}</p>
                   </section>
                 </div>
+
               </div>
             )
         })}
